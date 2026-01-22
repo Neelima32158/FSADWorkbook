a80086f9a003f338b3014b94fb8436801b5f6494
Neelima32158 <2400032158@kluniversity.in>
2026-01-22 17:10:49 +0530

    completed

---
+++ skil5/src/test/java/com/klu/Skil5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.klu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Skil5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
