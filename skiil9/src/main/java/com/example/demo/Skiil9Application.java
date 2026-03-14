dcfb0a39da729938d5fdecd502f92366300db7b8
Neelima32158 <2400032158@kluniversity.in>
2026-03-14 16:31:11 +0530

    completed

---
+++ skiil9/src/main/java/com/example/demo/Skiil9Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication(scanBasePackages = "com.klu")
+public class Skiil9Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Skiil9Application.class, args);
+    }
+
+}
