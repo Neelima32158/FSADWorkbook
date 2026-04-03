7dd0d19465bebc66a19c57a80d92ed5a12c9ff52
Neelima32158 <2400032158@kluniversity.in>
2026-04-03 19:45:41 +0530

    skill12

---
+++ skil12/src/main/java/com/example/demo/Skil12Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Skil12Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Skil12Application.class, args);
+	}
+
+}
